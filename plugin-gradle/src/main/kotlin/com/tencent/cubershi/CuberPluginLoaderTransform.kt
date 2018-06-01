237fbb1917300bb064590d17964d7da770b7cf8a
shifujun <shifujun@foxmail.com>
2018-06-01 17:05:03 +0800

    用CuberPluginLoaderTransform将demo-plugin中继承自Activity类的类的父类都改成MockActivity.

--- plugin-gradle/src/main/kotlin/com/tencent/cubershi/CuberPluginLoaderTransform.kt
+++ plugin-gradle/src/main/kotlin/com/tencent/cubershi/CuberPluginLoaderTransform.kt
@@ -1,5 +1,9 @@
 package com.tencent.cubershi
 
+import com.android.build.api.transform.TransformInvocation
+import javassist.ClassPool
+import javassist.CtClass
+import java.io.DataOutputStream
 import java.io.InputStream
 import java.io.OutputStream
 import java.util.function.BiConsumer
@@ -7,8 +11,30 @@ import java.util.function.BiConsumer
 class CuberPluginLoaderTransform : CustomClassTransform() {
     override fun loadTransformFunction(): BiConsumer<InputStream, OutputStream> =
             BiConsumer { input, output ->
-                input.copyTo(output)
+                val classPool: ClassPool = ClassPool.getDefault()
+                val mockActivityClass: CtClass = classPool.makeClass("com.tencent.cubershi.mock_interface.MockActivity")
+                val ctClass: CtClass = classPool.makeClass(input, false)
+//                if (ctClass.classFile.name == "android.support.v4.app.SupportActivity") {
+//                    ctClass.superclass = mockActivityClass
+//                }
+//                ctClass.toBytecode(DataOutputStream(output))
+
+                val superclass = ctClass.classFile.superclass
+                if (superclass != null
+                        && superclass == "android.app.Activity") {
+                    System.out.println("找到一个Activity:" + ctClass.name)
+                    ctClass.superclass = mockActivityClass
+                }
+                ctClass.toBytecode(DataOutputStream(output))
             }
+
+    override fun transform(invocation: TransformInvocation) {
+        System.out.println("CuberPluginLoaderTransform开始")
+
+        super.transform(invocation)
+
+        System.out.println("CuberPluginLoaderTransform结束")
+    }
 
     override fun getName(): String = "CuberPluginLoaderTransform"
 
