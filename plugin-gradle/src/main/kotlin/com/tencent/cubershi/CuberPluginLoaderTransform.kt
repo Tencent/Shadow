eb6688f79acb030d9b5b4c7d2a8c1fc7eb371dce
shifujun <shifujun@foxmail.com>
2018-06-02 11:49:58 +0800

    把所有对Activity的调用，除了<init>以外，都改为对MockActivity的调用。

--- plugin-gradle/src/main/kotlin/com/tencent/cubershi/CuberPluginLoaderTransform.kt
+++ plugin-gradle/src/main/kotlin/com/tencent/cubershi/CuberPluginLoaderTransform.kt
@@ -1,8 +1,14 @@
 package com.tencent.cubershi
 
 import com.android.build.api.transform.TransformInvocation
+import javassist.CannotCompileException
 import javassist.ClassPool
+import javassist.CodeConverter
 import javassist.CtClass
+import javassist.bytecode.BadBytecode
+import javassist.bytecode.ConstPool
+import javassist.bytecode.MethodInfo
+import javassist.convert.Transformer
 import java.io.DataOutputStream
 import java.io.InputStream
 import java.io.OutputStream
@@ -14,16 +20,14 @@ class CuberPluginLoaderTransform : CustomClassTransform() {
                 val classPool: ClassPool = ClassPool.getDefault()
                 val mockActivityClass: CtClass = classPool.makeClass("com.tencent.cubershi.mock_interface.MockActivity")
                 val ctClass: CtClass = classPool.makeClass(input, false)
-//                if (ctClass.classFile.name == "android.support.v4.app.SupportActivity") {
-//                    ctClass.superclass = mockActivityClass
-//                }
-//                ctClass.toBytecode(DataOutputStream(output))
 
-                val superclass = ctClass.classFile.superclass
-                if (superclass != null
-                        && superclass == "android.app.Activity") {
-                    System.out.println("找到一个Activity:" + ctClass.name)
-                    ctClass.superclass = mockActivityClass
+                val codeConverter = MyCodeConverter()
+
+                codeConverter.replaceCallOnClass("android.app.Activity", "com.tencent.cubershi.mock_interface.MockActivity")
+                try {
+                    ctClass.instrument(codeConverter)
+                } catch (ignored: Exception) {
+                    ignored.printStackTrace()
                 }
                 ctClass.toBytecode(DataOutputStream(output))
             }
@@ -38,4 +42,46 @@ class CuberPluginLoaderTransform : CustomClassTransform() {
 
     override fun getName(): String = "CuberPluginLoaderTransform"
 
+
+    class MyCodeConverter : CodeConverter() {
+        fun replaceCallOnClass(className: String, newClassName: String) {
+            transformers = TransformAllClassCall(transformers, className, newClassName)
+        }
+
+        /**
+         * 去掉了rebuildStackMap步骤
+         */
+        override fun doit(clazz: CtClass, minfo: MethodInfo, cp: ConstPool) {
+            var t: Transformer?
+            val codeAttr = minfo.getCodeAttribute()
+            if (codeAttr == null || transformers == null)
+                return
+            t = transformers
+            while (t != null) {
+                t.initialize(cp, clazz, minfo)
+                t = t.next
+            }
+
+            val iterator = codeAttr.iterator()
+            while (iterator.hasNext()) {
+                try {
+                    var pos = iterator.next()
+                    t = transformers
+                    while (t != null) {
+                        pos = t.transform(clazz, pos, iterator, cp)
+                        t = t.next
+                    }
+                } catch (e: BadBytecode) {
+                    throw CannotCompileException(e)
+                }
+
+            }
+
+            t = transformers
+            while (t != null) {
+                t.clean()
+                t = t.next
+            }
+        }
+    }
 }
---
+++ plugin-gradle/src/main/kotlin/com/tencent/cubershi/TransformAllClassCall.kt
@@ -0,0 +1,97 @@
+package com.tencent.cubershi
+
+import javassist.CtClass
+import javassist.bytecode.BadBytecode
+import javassist.bytecode.CodeIterator
+import javassist.bytecode.ConstPool
+import javassist.bytecode.Opcode
+import javassist.convert.Transformer
+
+
+class TransformAllClassCall(
+        next: Transformer?
+        , val classname: String
+        , val newClassname: String
+) : Transformer(next) {
+
+    /**
+     * Modify INVOKEINTERFACE, INVOKESPECIAL, INVOKESTATIC and INVOKEVIRTUAL
+     * so that a different method is invoked.  The class name in the operand
+     * of these instructions might be a subclass of the target class specified
+     * by `classname`.   This method transforms the instruction
+     * in that case unless the subclass overrides the target method.
+     */
+    @Throws(BadBytecode::class)
+    override fun transform(clazz: CtClass, pos: Int, iterator: CodeIterator,
+                           cp: ConstPool): Int {
+        var pos = pos
+        val c = iterator.byteAt(pos)
+        if (c == Opcode.INVOKEINTERFACE || c == Opcode.INVOKESPECIAL
+                || c == Opcode.INVOKESTATIC || c == Opcode.INVOKEVIRTUAL) {
+            val index = iterator.u16bitAt(pos + 1)
+
+            try {
+                val methodrefClassName = cp.getMethodrefClassName(index)
+                val methodrefNameAndType = cp.getMethodrefNameAndType(index)
+                val nameAndTypeName = cp.getNameAndTypeName(methodrefNameAndType)
+                val methodrefName = cp.getUtf8Info(nameAndTypeName)
+                val memberNameAndType = cp.getMemberNameAndType(index)
+                val nameAndTypeDescriptor = cp.getNameAndTypeDescriptor(memberNameAndType)
+
+                if (methodrefClassName == classname && methodrefName != "<init>") {
+                    val log = StringBuilder()
+                    log.append("In class:[").append(clazz.classFile.name)
+                    log.append("],it calls ").append(printIndex(cp, index))
+
+                    System.out.println(log.toString())
+
+
+                    pos = replace(pos, iterator, nameAndTypeName, nameAndTypeDescriptor, cp)
+
+                }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        return pos
+    }
+
+    @Throws(BadBytecode::class)
+    private fun replace(pos: Int, iterator: CodeIterator,
+                        methodname: Int,
+                        nameAndTypeDescriptor: Int, cp: ConstPool): Int {
+        val nt = cp.addNameAndTypeInfo(methodname, nameAndTypeDescriptor)
+        val ci = cp.addClassInfo(newClassname)
+
+        val newIndex = cp.addMethodrefInfo(ci, nt)
+
+        System.out.println("new Method:" + printIndex(cp, newIndex))
+
+        iterator.write16bit(newIndex, pos + 1)
+        return pos
+    }
+
+    private fun printIndex(cp: ConstPool, index: Int): String {
+        try {
+            val methodrefClassName = cp.getMethodrefClassName(index)
+            val methodrefNameAndType = cp.getMethodrefNameAndType(index)
+            val nameAndTypeName = cp.getNameAndTypeName(methodrefNameAndType)
+            val methodrefName = cp.getUtf8Info(nameAndTypeName)
+            val memberNameAndType = cp.getMemberNameAndType(index)
+            val nameAndTypeDescriptor = cp.getNameAndTypeDescriptor(memberNameAndType)
+            val methodSignature = cp.getUtf8Info(nameAndTypeDescriptor)
+
+            val methodStringBuilder = StringBuilder()
+            methodStringBuilder.append(methodrefClassName)
+            methodStringBuilder.append("#").append(methodrefName)
+            methodStringBuilder.append(" ").append(methodSignature)
+
+            return methodStringBuilder.toString()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return ""
+        }
+    }
+}
