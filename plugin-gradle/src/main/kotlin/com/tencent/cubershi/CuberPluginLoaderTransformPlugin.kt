702fbc6a3cc4c5c44878cd5b34f8c9dafdf6cdcf
shifujun <shifujun@foxmail.com>
2018-06-07 13:15:14 +0800

    将所有对Fragment.getActivity()的调用都改为对PluginFragment.getPluginActivity()的调用.

--- plugin-gradle/src/main/kotlin/com/tencent/cubershi/CuberPluginLoaderTransformPlugin.kt
+++ plugin-gradle/src/main/kotlin/com/tencent/cubershi/CuberPluginLoaderTransformPlugin.kt
@@ -1,14 +1,22 @@
 package com.tencent.cubershi
 
 import com.android.build.gradle.AppPlugin
+import javassist.ClassPool
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import java.io.File
 
 class CuberPluginLoaderTransformPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         System.err.println("CuberPluginLoaderTransformPlugin project.name==" + project.name)
+
+        val classPool: ClassPool = ClassPool.getDefault()
         val plugin = project.plugins.getPlugin(AppPlugin::class.java)
+
+        val androidJar = File(plugin.extension.sdkDirectory, "platforms/${plugin.extension.compileSdkVersion}/android.jar")
+        classPool.appendClassPath(androidJar.absolutePath)
+
         plugin.extension.registerTransform(CuberPluginLoaderTransform())
     }
 }
