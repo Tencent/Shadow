d39f96872418c8da71053801db0ba41f1d8d7bde
shifujun <shifujun@foxmail.com>
2018-06-08 21:59:47 +0800

    还原对Fragment的修改。准备换一个Fragment支持方案。

--- plugin-gradle/src/main/kotlin/com/tencent/cubershi/CuberPluginLoaderTransform.kt
+++ plugin-gradle/src/main/kotlin/com/tencent/cubershi/CuberPluginLoaderTransform.kt
@@ -4,8 +4,8 @@ import com.android.SdkConstants
 import com.android.build.api.transform.TransformInvocation
 import com.android.utils.FileUtils
 import com.tencent.cubershi.special.SpecialTransform
-import com.tencent.cubershi.special.android.arch.lifecycle.ReportFragmentTransform
-import javassist.*
+import javassist.ClassPool
+import javassist.CtClass
 import java.io.DataOutputStream
 import java.io.File
 import java.io.InputStream
@@ -20,26 +20,10 @@ class CuberPluginLoaderTransform() : CustomClassTransform() {
         const val MockApplicationClassname = "com.tencent.cubershi.mock_interface.MockApplication"
         const val AndroidActivityClassname = "android.app.Activity"
         const val MockActivityClassname = "com.tencent.cubershi.mock_interface.MockActivity"
-        const val AndroidFragmentClassname = "android.app.Fragment"
-        const val MockFragmentClassname = "com.tencent.cubershi.mock_interface.PluginFragment"
         const val AndroidServiceClassname = "android.app.Service"
         const val MockServiceClassname = "com.tencent.cubershi.mock_interface.MockService"
-        const val PluginFragmentClassname = "com.tencent.cubershi.mock_interface.PluginFragment"
         val SpecialTransformMap = mapOf<String, SpecialTransform>(
-                "android.arch.lifecycle.ReportFragment" to ReportFragmentTransform()
         )
-        val AndroidFragmentCtClass: CtClass
-        val PluginFragmentCtClass: CtClass
-        val AndroidFragmentGetActivityCtMethod: CtMethod
-        val PluginFragmentGetPluginActivityCtMethod: CtMethod
-
-        init {
-            val cp = ClassPool.getDefault()
-            AndroidFragmentCtClass = cp[AndroidFragmentClassname]
-            PluginFragmentCtClass = cp[PluginFragmentClassname]
-            AndroidFragmentGetActivityCtMethod = AndroidFragmentCtClass.methods.find { it.name == "getActivity" }!!
-            PluginFragmentGetPluginActivityCtMethod = PluginFragmentCtClass.methods.find { it.name == "getPluginActivity" }!!
-        }
     }
 
     override fun loadTransformFunction(): BiConsumer<InputStream, OutputStream> =
@@ -51,45 +35,10 @@ class CuberPluginLoaderTransform() : CustomClassTransform() {
                 } else {
                     ctClass.replaceClassName(AndroidActivityClassname, MockActivityClassname)
                     ctClass.replaceClassName(AndroidApplicationClassname, MockApplicationClassname)
-
-                    if (ctClass.classFile.superclass == AndroidFragmentClassname) {
-                        ctClass.classFile.superclass = MockFragmentClassname
-                    }
-                    val allRefClassesCanBeFound = ctClass.refClasses.all {
-                        var found: Boolean;
-                        try {
-                            classPool[it as String]
-                            found = true
-                        } catch (e: NotFoundException) {
-                            found = false
-                        }
-                        found
-                    }
-                    if (allRefClassesCanBeFound) {
-                        redirectFragmentCallGetActivity(ctClass)
-                    }
                     ctClass.replaceClassName(AndroidServiceClassname, MockServiceClassname)
                 }
                 ctClass.toBytecode(DataOutputStream(output))
             }
-
-    /**
-     * 替换所有Fragment.getActivity()为PluginFragment.getPluginActivity()
-     */
-    fun redirectFragmentCallGetActivity(ctClass: CtClass) {
-        val origMethod = AndroidFragmentGetActivityCtMethod
-        val substMethod = PluginFragmentGetPluginActivityCtMethod
-        origMethod.methodInfo.descriptor = substMethod.methodInfo.descriptor
-        val codeConverter = CodeConverter()
-        try {
-            codeConverter.redirectMethodCall(origMethod, substMethod)
-        } catch (e: CannotCompileException) {
-            System.err.println("origMethod signature:" + origMethod.methodInfo.descriptor)
-            System.err.println("substMethod signature:" + substMethod.methodInfo.descriptor)
-            throw e
-        }
-        ctClass.instrument(codeConverter)
-    }
 
     override fun transform(invocation: TransformInvocation) {
         System.out.println("CuberPluginLoaderTransform开始")
