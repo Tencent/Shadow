39271754ed355a7753e3ba863ccd80237d68fd88
kakawar <cluoya@sina.com>
2018-06-21 15:17:01 +0800

    多dex修改

---
+++ multidex/src/main/java/com/tencent/hydevteam/pluginframework/pluginloader/multidex/MultiDexClassLoader.java
@@ -0,0 +1,23 @@
+package com.tencent.hydevteam.pluginframework.pluginloader.multidex;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.tencent.hydevteam.pluginframework.pluginloader.multidex.MultiDex;
+
+import java.io.File;
+
+import dalvik.system.DexClassLoader;
+
+/**
+ * Created by cubershi on 2016/12/20.
+ */
+
+public class MultiDexClassLoader extends DexClassLoader {
+
+    public MultiDexClassLoader(Context hostContext, String dexPath, String optimizedDirectory, String libraryPath, ClassLoader parent) {
+        super(dexPath, optimizedDirectory, libraryPath, parent);
+        SharedPreferences pluginLoaderMultiDex = hostContext.getSharedPreferences("PluginLoaderMultiDex", Context.MODE_PRIVATE);
+        MultiDex.install(this, dexPath, new File(optimizedDirectory), pluginLoaderMultiDex);
+    }
+}
