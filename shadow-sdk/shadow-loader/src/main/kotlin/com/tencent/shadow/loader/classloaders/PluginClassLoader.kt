5cc30047dc4dde4e3072aa23b8efc46196291c28
shifujun <shifujun@foxmail.com>
2018-07-25 14:13:19 +0800

    PluginClassLoader添加白名单过滤runtime类。避免和host类冲突。

--- shadow-sdk/shadow-loader/src/main/kotlin/com/tencent/shadow/loader/classloaders/PluginClassLoader.kt
+++ shadow-sdk/shadow-loader/src/main/kotlin/com/tencent/shadow/loader/classloaders/PluginClassLoader.kt
@@ -16,26 +16,49 @@ class PluginClassLoader(
         hostAppContext: Context, dexPath: String, optimizedDirectory: String, private val librarySearchPath: String, parent: ClassLoader
 ) : DexClassLoader(dexPath, optimizedDirectory, librarySearchPath, parent) {
 
+    private val mGrandParent: ClassLoader
+
     init {
         if (Build.VERSION.SDK_INT <= MultiDex.MAX_SUPPORTED_SDK_VERSION) {
             val pluginLoaderMultiDex = hostAppContext.getSharedPreferences("com.tencent.shadow.multidex", Context.MODE_PRIVATE)
             MultiDex.install(this, dexPath, File(optimizedDirectory), pluginLoaderMultiDex)
         }
+        mGrandParent = parent.parent
     }
 
     fun getLibrarySearchPath() = librarySearchPath
 
     @Throws(ClassNotFoundException::class)
     override fun loadClass(className: String, resolve: Boolean): Class<*> {
-        try {
+        if (className.startsWith("com.tencent.shadow.runtime")
+                || className.startsWith("org.apache.commons.logging")//org.apache.commons.logging是非常特殊的的包,由系统放到App的PathClassLoader中.
+        ) {
             return super.loadClass(className, resolve)
-        } catch (e: ClassNotFoundException) {
-            //org.apache.commons.logging是非常特殊的的包,由系统放到App的PathClassLoader中.
-            if (className.startsWith("org.apache.commons.logging")) {
-                return super.loadClass(className)
-            } else {
-                throw e
+        } else {
+            var clazz: Class<*>? = findLoadedClass(className)
+
+            if (clazz == null) {
+                var suppressed: ClassNotFoundException? = null
+                try {
+                    clazz = findClass(className)!!
+                } catch (e: ClassNotFoundException) {
+                    suppressed = e
+                }
+
+                if (clazz == null) {
+                    try {
+                        clazz = mGrandParent.loadClass(className)!!
+                    } catch (e: ClassNotFoundException) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                            e.addSuppressed(suppressed)
+                        }
+                        throw e
+                    }
+
+                }
             }
+
+            return clazz
         }
     }
 }
