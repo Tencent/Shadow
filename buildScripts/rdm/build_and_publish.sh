df39c68baeb4b3181e5d02b5b17cec23188fdf8c
shifujun <shifujun@foxmail.com>
2018-09-18 14:39:50 +0800

    简化shell脚本，将构建sdk的任务写在gradle脚本里。

--- buildScripts/rdm/build_and_publish.sh
+++ buildScripts/rdm/build_and_publish.sh
@@ -2,8 +2,6 @@ export ANDROID_HOME=$ANDROID_SDK
 export JAVA_HOME=$JDK8
 export PATH=$JDK8/bin:$GRADLE_HOME/bin:$PATH
 
-export SHORT_REV=$(git rev-parse --short $GIT_COMMIT)
-
 function checkError()
 {
 #如果前一条命令出错了则终止构建
@@ -15,25 +13,12 @@ fi
 
 rm bin/*.*
 
-if   [   $clean   ];
-then
-sh gradlew clean
-checkError
-fi
-
-sh gradlew --daemon :shadow-sdk:shadow-loader:publishReleasePublicationToMavenRepository
-checkError
-sh gradlew --daemon :shadow-sdk:shadow-runtime:publishReleasePublicationToMavenRepository
-checkError
-sh gradlew --daemon :shadow-sdk:shadow-transform:publishReleasePublicationToMavenRepository
+sh gradlew --daemon cleanThenPublishSdkToMaven
 checkError
 
 cd $WORKSPACE/bin
 
-if   [   $PUBLISH_RELEASE   ];
-then
-touch PUBLISH_SUCCESS
-fi
+touch BUILD_DONE
 
 
 
