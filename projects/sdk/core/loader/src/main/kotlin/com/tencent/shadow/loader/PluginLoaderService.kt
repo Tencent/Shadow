8fde1ab6fd64f4b60d5b6c0e05629715666f7280
guok2046 <guok2046@gmail.com>
2018-11-29 14:38:46 +0800

    PluginLoaderService 改成 IBinder

--- projects/sdk/core/loader/src/main/kotlin/com/tencent/shadow/loader/PluginLoaderService.kt
+++ projects/sdk/core/loader/src/main/kotlin/com/tencent/shadow/loader/PluginLoaderService.kt
@@ -5,42 +5,20 @@ import android.os.IBinder
 import android.os.RemoteException
 import com.tencent.hydevteam.common.classloader.ApkClassLoader
 import com.tencent.hydevteam.pluginframework.pluginloader.PluginLoader
-import com.tencent.shadow.runtime.ShadowService
+import com.tencent.shadow.sdk.service.IPluginLoaderServiceInterface
 
-class PluginLoaderService : ShadowService() {
+open class PluginLoaderService : IPluginLoaderServiceInterface.Stub() {
+
+    companion object {
+
+        private val CLASSS_PLUGIN_LOADER_IMPL = "com.tencent.shadow.sdk.pluginloader.pluginLoaderImpl"
+    }
 
     private var mPluginLoader: PluginLoader? = null
 
     private val mApkClassLoader = PluginLoaderService::class.java.classLoader as ApkClassLoader
 
-    private val mBinder = object : IPluginLoaderServiceInterface.Stub() {
-        @Throws(RemoteException::class)
-        override fun loadPlugin(partKey: String) {
-
-        }
-
-        @Throws(RemoteException::class)
-        override fun callApplicationOnCreate(partKey: String) {
-
-        }
-
-        @Throws(RemoteException::class)
-        override fun convertActivityIntent(pluginActivityIntent: Intent): Intent? {
-            return null
-        }
-
-        @Throws(RemoteException::class)
-        override fun startPluginService(pluginServiceIntent: Intent) {
-
-        }
-
-        @Throws(RemoteException::class)
-        override fun bindPluginService(pluginServiceIntent: Intent): IBinder? {
-            return null
-        }
-    }
-
-    override fun onBind(intent: Intent): IBinder? {
+    init {
         if (mPluginLoader != null) {
             try {
                 mPluginLoader = mApkClassLoader.getInterface(PluginLoader::class.java, CLASSS_PLUGIN_LOADER_IMPL)
@@ -49,11 +27,31 @@ class PluginLoaderService : ShadowService() {
             }
 
         }
-        return mBinder
     }
 
-    companion object {
+    @Throws(RemoteException::class)
+    override fun loadPlugin(partKey: String) {
 
-        private val CLASSS_PLUGIN_LOADER_IMPL = "com.tencent.shadow.sdk.pluginloader.pluginLoaderImpl"
     }
+
+    @Throws(RemoteException::class)
+    override fun callApplicationOnCreate(partKey: String) {
+
+    }
+
+    @Throws(RemoteException::class)
+    override fun convertActivityIntent(pluginActivityIntent: Intent): Intent? {
+        return null
+    }
+
+    @Throws(RemoteException::class)
+    override fun startPluginService(pluginServiceIntent: Intent) {
+
+    }
+
+    @Throws(RemoteException::class)
+    override fun bindPluginService(pluginServiceIntent: Intent): IBinder? {
+        return null
+    }
+
 }
