568544bbf496debfaf251889d150a2be2dccdbcc
coolbluesky <chenjianping129@126.com>
2018-12-06 21:23:12 +0800

    插件间共用接口classloader实现，它负责加载插件间自定义的一些接口，共用类，以使插件间可以按照他们自定义的接口通信

---
+++ projects/sdk/core/loader/src/main/kotlin/com/tencent/shadow/loader/classloaders/InterfaceClassLoader.kt
@@ -0,0 +1,59 @@
+package com.tencent.shadow.loader.classloaders
+
+import android.os.Build
+
+/**
+ * 公用接口classloader,它加载插件间共用的接口，它将作为所有插件classloader的
+ * 父classloader
+ * Created by jaylanchen on 2018/12/5.
+ */
+class InterfaceClassLoader(parent: ClassLoader) : ClassLoader(parent) {
+
+    private val mGrandParent = parent.parent
+    private val mInterfaceClassLoaderList = ArrayList<ClassLoader>()
+
+
+    @Synchronized
+    fun addInterfaceClassLoader(classLoader: ClassLoader) {
+        mInterfaceClassLoaderList.add(classLoader)
+    }
+
+    override fun loadClass(className: String, resolve: Boolean): Class<*> {
+        if (className.startsWith("com.tencent.shadow.runtime")
+                || className.startsWith("org.apache.commons.logging")//org.apache.commons.logging是非常特殊的的包,由系统放到App的PathClassLoader中.
+                || (Build.VERSION.SDK_INT < 28 && className.startsWith("org.apache.http"))) {//Android 9.0以下的系统里面带有http包，走系统的不走本地的
+            return super.loadClass(className, resolve)
+        } else {
+
+            var clazz: Class<*>? = null
+
+            for (classLoader in mInterfaceClassLoaderList) {
+                try {
+                    clazz = classLoader.loadClass(className)
+                    if (clazz != null) {
+                        break
+                    }
+                } catch (e: ClassNotFoundException) {
+
+                }
+            }
+
+
+            if (clazz == null) {
+                try {
+                    clazz = mGrandParent.loadClass(className)
+                } catch (e: ClassNotFoundException) {
+                }
+            }
+
+            if (clazz == null) {
+                // 还是找不到
+                throw ClassNotFoundException(className)
+            }
+
+            return clazz
+        }
+
+    }
+
+}
