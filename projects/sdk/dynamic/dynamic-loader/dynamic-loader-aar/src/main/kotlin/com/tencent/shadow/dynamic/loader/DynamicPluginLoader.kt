544fdea8035c9d4bb58a4b3efce6f74763cefd0b
shifujun <shifujun@foxmail.com>
2018-12-15 22:15:27 +0800

    DynamicPluginLoader改为internal类

--- projects/sdk/dynamic/dynamic-loader/dynamic-loader-aar/src/main/kotlin/com/tencent/shadow/dynamic/loader/DynamicPluginLoader.kt
+++ projects/sdk/dynamic/dynamic-loader/dynamic-loader-aar/src/main/kotlin/com/tencent/shadow/dynamic/loader/DynamicPluginLoader.kt
@@ -14,7 +14,7 @@ import com.tencent.shadow.dynamic.host.ApkClassLoader
 import com.tencent.shadow.runtime.container.*
 import java.util.concurrent.CountDownLatch
 
-open class DynamicPluginLoader(hostContext: Context) : PluginLoader.Stub(), DelegateProvider {
+internal class DynamicPluginLoader(hostContext: Context) : PluginLoader.Stub(), DelegateProvider {
 
     companion object {
 
