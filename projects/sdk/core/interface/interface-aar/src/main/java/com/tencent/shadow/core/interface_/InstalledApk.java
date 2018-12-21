1375fe1c253b4c3147490083b2e1325d4faf09ec
shifujun <shifujun@foxmail.com>
2018-12-21 10:59:45 +0800

    InstalledApk添加一个extras字段。

--- projects/sdk/core/interface/interface-aar/src/main/java/com/tencent/shadow/core/interface_/InstalledApk.java
+++ projects/sdk/core/interface/interface-aar/src/main/java/com/tencent/shadow/core/interface_/InstalledApk.java
@@ -14,16 +14,30 @@ public class InstalledApk implements Parcelable {
 
     public final String libraryPath;
 
+    public final byte[] parcelExtras;
+
     public InstalledApk(String apkFilePath, String oDexPath, String libraryPath) {
+        this(apkFilePath, oDexPath, libraryPath, null);
+    }
+
+    public InstalledApk(String apkFilePath, String oDexPath, String libraryPath, byte[] parcelExtras) {
         this.apkFilePath = apkFilePath;
         this.oDexPath = oDexPath;
         this.libraryPath = libraryPath;
+        this.parcelExtras = parcelExtras;
     }
 
     protected InstalledApk(Parcel in) {
         apkFilePath = in.readString();
         oDexPath = in.readString();
         libraryPath = in.readString();
+        int parcelExtrasLength = in.readInt();
+        if (parcelExtrasLength > 0) {
+            parcelExtras = new byte[parcelExtrasLength];
+        } else {
+            parcelExtras = null;
+        }
+        in.readByteArray(parcelExtras);
     }
 
     @Override
@@ -31,6 +45,8 @@ public class InstalledApk implements Parcelable {
         dest.writeString(apkFilePath);
         dest.writeString(oDexPath);
         dest.writeString(libraryPath);
+        dest.writeInt(parcelExtras == null ? 0 : parcelExtras.length);
+        dest.writeByteArray(parcelExtras);
     }
 
     @Override
