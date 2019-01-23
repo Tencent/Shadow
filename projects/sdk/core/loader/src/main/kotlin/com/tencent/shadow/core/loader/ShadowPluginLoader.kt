a879235746d3020657e5f00ca482b4567271b43a
guok2046 <guok2046@gmail.com>
2019-01-23 16:35:40 +0800

    Revert "临时修复core.loader直接使用"
    This reverts commit 189b4b5b

--- projects/sdk/core/loader/src/main/kotlin/com/tencent/shadow/core/loader/ShadowPluginLoader.kt
+++ projects/sdk/core/loader/src/main/kotlin/com/tencent/shadow/core/loader/ShadowPluginLoader.kt
@@ -67,7 +67,7 @@ abstract class ShadowPluginLoader(hostAppContext: Context) : DelegateProvider, D
 
     private val mPluginServiceManagerLock = ReentrantLock()
 
-    private val mInterfaceClassLoader = InterfaceClassLoader(ShadowPluginLoader::class.java.classLoader)//todo cubershi ：这个东西的父类不能固定成.parent。否则core层就不能单独使用。
+    private val mInterfaceClassLoader = InterfaceClassLoader(ShadowPluginLoader::class.java.classLoader.parent)
 
     /**
      * 插件将要使用的so的ABI，Loader会将其从apk中解压出来。
