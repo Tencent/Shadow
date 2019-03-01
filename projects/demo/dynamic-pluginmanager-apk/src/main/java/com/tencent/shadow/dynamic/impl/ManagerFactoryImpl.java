aa459142c17e4feae2e25dd353fe8ad755bba7ef
guok2046 <guok2046@gmail.com>
2019-03-01 09:29:23 +0800

    添加注释

--- projects/demo/dynamic-pluginmanager-apk/src/main/java/com/tencent/shadow/dynamic/impl/ManagerFactoryImpl.java
+++ projects/demo/dynamic-pluginmanager-apk/src/main/java/com/tencent/shadow/dynamic/impl/ManagerFactoryImpl.java
@@ -6,6 +6,9 @@ import com.tencent.shadow.dynamic.host.ManagerFactory;
 import com.tencent.shadow.dynamic.host.PluginManagerImpl;
 import com.tencent.shadow.manager.DemoPluginManager;
 
+/**
+ * 此类包名及类名固定
+ */
 public final class ManagerFactoryImpl implements ManagerFactory {
     @Override
     public PluginManagerImpl buildManager(Context context) {
--- projects/demo/dynamic-pluginmanager-apk/src/main/java/com/tencent/shadow/dynamic/impl/WhiteList.java
+++ projects/demo/dynamic-pluginmanager-apk/src/main/java/com/tencent/shadow/dynamic/impl/WhiteList.java
@@ -1,6 +1,7 @@
 package com.tencent.shadow.dynamic.impl;
 
 /**
+ * 此类包名及类名固定
  * classLoader的白名单
  * PluginManager可以加载宿主中位于白名单内的类
  */
