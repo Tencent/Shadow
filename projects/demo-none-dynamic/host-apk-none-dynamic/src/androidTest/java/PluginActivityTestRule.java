d5f5c3bf2585409d621dfc4e4f7431ccacfe1d41
shifujun <shifujun@foxmail.com>
2019-03-20 16:21:47 +0800

    非动态的Demo中将插件加载重构到Application类中，方便后面在AndroidTest中调用。

--- projects/demo-none-dynamic/host-apk-none-dynamic/src/androidTest/java/PluginActivityTestRule.java
+++ projects/demo-none-dynamic/host-apk-none-dynamic/src/androidTest/java/PluginActivityTestRule.java
@@ -1,32 +1,48 @@
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.intercepting.SingleActivityFactory;
 
 public class PluginActivityTestRule<T extends Activity> extends ActivityTestRule<T> {
-    public PluginActivityTestRule(Class<T> activityClass) {
+    final private Intent mStartIntent;
+
+    public static ActivityTestRule<?> build(Intent pluginIntent) {
+        Context targetContext = InstrumentationRegistry.getTargetContext();
+        String packageName = targetContext.getPackageName();
+        Context applicationContext = targetContext.getApplicationContext();
+        throw new UnsupportedOperationException();
+    }
+
+    public PluginActivityTestRule(Class<T> activityClass, Intent mStartIntent) {
         super(activityClass);
+        this.mStartIntent = mStartIntent;
     }
 
-    public PluginActivityTestRule(Class<T> activityClass, boolean initialTouchMode) {
+    public PluginActivityTestRule(Class<T> activityClass, boolean initialTouchMode, Intent mStartIntent) {
         super(activityClass, initialTouchMode);
+        this.mStartIntent = mStartIntent;
     }
 
-    public PluginActivityTestRule(Class<T> activityClass, boolean initialTouchMode, boolean launchActivity) {
+    public PluginActivityTestRule(Class<T> activityClass, boolean initialTouchMode, boolean launchActivity, Intent mStartIntent) {
         super(activityClass, initialTouchMode, launchActivity);
+        this.mStartIntent = mStartIntent;
     }
 
-    public PluginActivityTestRule(SingleActivityFactory<T> activityFactory, boolean initialTouchMode, boolean launchActivity) {
+    public PluginActivityTestRule(SingleActivityFactory<T> activityFactory, boolean initialTouchMode, boolean launchActivity, Intent mStartIntent) {
         super(activityFactory, initialTouchMode, launchActivity);
+        this.mStartIntent = mStartIntent;
     }
 
-    public PluginActivityTestRule(Class<T> activityClass, @NonNull String targetPackage, int launchFlags, boolean initialTouchMode, boolean launchActivity) {
+    public PluginActivityTestRule(Class<T> activityClass, @NonNull String targetPackage, int launchFlags, boolean initialTouchMode, boolean launchActivity, Intent mStartIntent) {
         super(activityClass, targetPackage, launchFlags, initialTouchMode, launchActivity);
+        this.mStartIntent = mStartIntent;
     }
 
     @Override
     protected Intent getActivityIntent() {
-        return super.getActivityIntent();
+        return mStartIntent;
     }
 }
--- projects/demo-none-dynamic/host-apk-none-dynamic/src/main/java/com/tencent/shadow/demo/host/MainActivity.java
+++ projects/demo-none-dynamic/host-apk-none-dynamic/src/main/java/com/tencent/shadow/demo/host/MainActivity.java
@@ -6,24 +6,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Parcel;
 import android.view.View;
 
-import com.tencent.shadow.core.common.InstalledApk;
-import com.tencent.shadow.core.loader.LoadParameters;
-import com.tencent.shadow.runtime.ShadowApplication;
-import com.tencent.shadow.runtime.container.ContentProviderDelegateProviderHolder;
-import com.tencent.shadow.runtime.container.DelegateProviderHolder;
-
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import static com.tencent.shadow.demo.host.HostApplication.PART_MAIN;
 
 public class MainActivity extends Activity {
-
-    private static final PreparePluginApkBloc sDemoPluginPrepareBloc
-            = new PreparePluginApkBloc(
-            "plugin.apk"
-    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,37 +39,14 @@ public class MainActivity extends Activity {
     }
 
     public void startDemoPlugin(View view) {
-        InstalledApk installedApk = sDemoPluginPrepareBloc.preparePlugin(this.getApplicationContext());
+        HostApplication application = (HostApplication) getApplication();
+        application.loadPlugin(PART_MAIN);
 
-        LoadParameters loadParameters = new LoadParameters("partMain", 0, null);
+        Intent pluginIntent = new Intent();
+        pluginIntent.setClassName(getPackageName(), "com.tencent.shadow.demo.gallery.splash.SplashActivity");
 
-        Parcel parcel = Parcel.obtain();
-        loadParameters.writeToParcel(parcel, 0);
-        InstalledApk plugin = new InstalledApk(installedApk.apkFilePath, installedApk.oDexPath, installedApk.libraryPath, parcel.marshall());
-        parcel.recycle();
-
-        DemoPluginLoader pluginLoader = new DemoPluginLoader(getApplicationContext());
-        pluginLoader.onCreate();
-        DelegateProviderHolder.setDelegateProvider(pluginLoader);
-        ContentProviderDelegateProviderHolder.setContentProviderDelegateProvider(pluginLoader);
-
-        try {
-            Future<?> future = pluginLoader.loadPlugin(plugin);
-
-            future.get(10, TimeUnit.SECONDS);
-
-            pluginLoader.callApplicationOnCreate("partMain");
-
-            Intent pluginIntent = new Intent();
-            pluginIntent.setClassName("com.tencent.shadow.demo_host", "com.tencent.shadow.demo.gallery.splash.SplashActivity");
-
-            Intent intent = pluginLoader.getMComponentManager().convertPluginActivityIntent(pluginIntent);
-            startActivity(intent);
-
-        } catch (Exception e) {
-            throw new RuntimeException("加载失败", e);
-        }
-
+        Intent intent = application.getPluginLoader().getMComponentManager().convertPluginActivityIntent(pluginIntent);
+        startActivity(intent);
     }
 
 }
