bab9a2160799546eaefb2c1639acc3e5479d4f55
shifujun <shifujun@foxmail.com>
2019-03-20 16:47:10 +0800

    非动态的Demo中进行插件Activity的AndroidTest基本结构。可用 ./gradlew :projects:demo-none-dynamic:host-apk-none-dynamic:connectedDebugAndroidTest 进行测试。

---
+++ projects/demo-none-dynamic/host-apk-none-dynamic/src/androidTest/java/com/tencent/shadow/demo/host/BasicTest.java
@@ -0,0 +1,41 @@
+package com.tencent.shadow.demo.host;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class BasicTest {
+
+    @Rule
+    public ActivityTestRule<?> startActivityRule() {
+        Context applicationContext = InstrumentationRegistry.getTargetContext().getApplicationContext();
+        HostApplication application = (HostApplication) applicationContext;
+        application.loadPlugin(HostApplication.PART_MAIN);
+
+        String packageName = InstrumentationRegistry.getTargetContext().getPackageName();
+        Intent pluginIntent = new Intent();
+        pluginIntent.setClassName(
+                packageName,
+                "com.tencent.shadow.demo.usecases.activity.TestActivityOnCreate"
+        );
+        return PluginActivityTestRule.build(pluginIntent);
+    }
+
+
+    @Test
+    public void testBasicUsage() {
+        Espresso.onView(ViewMatchers.withTagValue(Matchers.<Object>is("tv_msg")))
+                .check(ViewAssertions.matches(ViewMatchers.withText("Activity生命周期测试")));
+    }
+}
--- projects/demo-none-dynamic/host-apk-none-dynamic/src/androidTest/java/com/tencent/shadow/demo/host/PluginActivityTestRule.java
+++ projects/demo-none-dynamic/host-apk-none-dynamic/src/androidTest/java/com/tencent/shadow/demo/host/PluginActivityTestRule.java
@@ -1,3 +1,5 @@
+package com.tencent.shadow.demo.host;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -6,14 +8,33 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.intercepting.SingleActivityFactory;
 
+import com.tencent.shadow.core.loader.ShadowPluginLoader;
+import com.tencent.shadow.core.loader.managers.ComponentManager;
+
 public class PluginActivityTestRule<T extends Activity> extends ActivityTestRule<T> {
     final private Intent mStartIntent;
 
     public static ActivityTestRule<?> build(Intent pluginIntent) {
         Context targetContext = InstrumentationRegistry.getTargetContext();
-        String packageName = targetContext.getPackageName();
         Context applicationContext = targetContext.getApplicationContext();
-        throw new UnsupportedOperationException();
+        HostApplication application = (HostApplication) applicationContext;
+        ShadowPluginLoader pluginLoader = application.getPluginLoader();
+        ComponentManager componentManager = pluginLoader.getComponentManager();
+
+        Intent intent = componentManager.convertPluginActivityIntent(pluginIntent);
+        if (intent == pluginIntent) {
+            throw new RuntimeException("插件中到不到" + pluginIntent.getComponent());
+        }
+
+        String containerActivityClassName = intent.getComponent().getClassName();
+        Class<? extends Activity> containerActivityClass;
+        try {
+            Class<?> aClass = Class.forName(containerActivityClassName);
+            containerActivityClass = aClass.asSubclass(Activity.class);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return new PluginActivityTestRule<>(containerActivityClass, intent);
     }
 
     public PluginActivityTestRule(Class<T> activityClass, Intent mStartIntent) {
