91ae9d90ae5b7f12e612e53cf4cb24c178fdfec4
shifujun <shifujun@foxmail.com>
2019-03-25 15:36:13 +0800

    回滚误提交的文件 Revert: 重构DemoPluginManager，使其可以方便的测试。 (f8696a0d)

--- projects/sdk/core/loader/src/main/kotlin/com/tencent/shadow/core/loader/blocs/CreateResourceBloc.kt
+++ projects/sdk/core/loader/src/main/kotlin/com/tencent/shadow/core/loader/blocs/CreateResourceBloc.kt
@@ -9,7 +9,6 @@ object CreateResourceBloc {
     fun create(packageArchiveInfo: PackageInfo, archiveFilePath: String, hostAppContext: Context): Resources {
         val packageManager = hostAppContext.packageManager
         packageArchiveInfo.applicationInfo.publicSourceDir = archiveFilePath
-        packageArchiveInfo.applicationInfo.sharedLibraryFiles
         try {
             return packageManager.getResourcesForApplication(packageArchiveInfo.applicationInfo)
         } catch (e: PackageManager.NameNotFoundException) {
