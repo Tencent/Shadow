10c73f1ecb9e1e323d9366e54a9bb3d10d65713a
guok2046 <guok2046@gmail.com>
2019-04-09 14:38:22 +0800

    采用监听布局高度变化的方式来监听是否发生了软键盘弹出，使用了一个中转的jumpActivity来接收弹出结果

--- projects/demo/plugin-app/demo-main/src/main/java/com/tencent/shadow/demo/usecases/activity/TestActivityWindowSoftMode.java
+++ projects/demo/plugin-app/demo-main/src/main/java/com/tencent/shadow/demo/usecases/activity/TestActivityWindowSoftMode.java
@@ -1,20 +1,51 @@
 package com.tencent.shadow.demo.usecases.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 
 import com.tencent.shadow.demo.gallery.BaseActivity;
 import com.tencent.shadow.demo.gallery.R;
+import com.tencent.shadow.demo.usecases.util.SoftKeyBoardListener;
 
 
 public class TestActivityWindowSoftMode extends BaseActivity {
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_softmode);
+
+        final Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                intent.putExtra("result","hide");
+                setResult(0,intent);
+                finish();
+            }
+        },3000);
+
+        SoftKeyBoardListener.setListener(this, new SoftKeyBoardListener.OnSoftKeyBoardChangeListener() {
+            @Override
+            public void keyBoardShow(int height) {
+                handler.removeCallbacksAndMessages(null);
+
+                Intent intent = new Intent();
+                intent.putExtra("result","show");
+                setResult(0,intent);
+                finish();
+            }
+            @Override
+            public void keyBoardHide(int height) {
+
+            }
+        });
+
     }
-
-
 
 }
---
+++ projects/demo/plugin-app/demo-main/src/main/java/com/tencent/shadow/demo/usecases/activity/WindowSoftModeJumpActivity.java
@@ -0,0 +1,40 @@
+package com.tencent.shadow.demo.usecases.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
+
+
+import com.tencent.shadow.demo.gallery.R;
+import com.tencent.shadow.demo.usecases.BaseAndroidTestActivity;
+
+public class WindowSoftModeJumpActivity extends BaseAndroidTestActivity {
+
+    private TextView mText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_common);
+        mText = findViewById(R.id.text);
+    }
+
+    public void doClick(View view){
+        mIdlingResource.setIdleState(false);
+        startActivityForResult(new Intent(this, TestActivityWindowSoftMode.class), 1001);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mIdlingResource.setIdleState(true);
+        String txt = data.getStringExtra("result");
+        mText.setText(txt);
+
+    }
+
+
+
+}
