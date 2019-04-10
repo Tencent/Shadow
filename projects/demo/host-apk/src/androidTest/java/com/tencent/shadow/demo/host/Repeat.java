727b7b8f6d3fba75bf9ff5a0e286d41739955598
shifujun <shifujun@foxmail.com>
2019-04-10 17:50:50 +0800

    添加Repeat功能，可重复单元测试多次。

---
+++ projects/demo/host-apk/src/androidTest/java/com/tencent/shadow/demo/host/Repeat.java
@@ -0,0 +1,14 @@
+package com.tencent.shadow.demo.host;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({METHOD, ANNOTATION_TYPE})
+public @interface Repeat {
+    int value() default 1;
+}
