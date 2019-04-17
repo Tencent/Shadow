556677b72ad420ec60247a97ab0ff7b5968a4cd1
guok2046 <guok2046@gmail.com>
2019-04-17 14:33:23 +0800

    service demo加入结果显示在TextView上

--- projects/demo/plugin-app/demo-main/src/main/java/com/tencent/shadow/demo/usecases/service/TestStartServiceActivity.java
+++ projects/demo/plugin-app/demo-main/src/main/java/com/tencent/shadow/demo/usecases/service/TestStartServiceActivity.java
@@ -1,20 +1,25 @@
 package com.tencent.shadow.demo.usecases.service;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
+import android.widget.TextView;
 
-import com.tencent.shadow.demo.gallery.BaseActivity;
 import com.tencent.shadow.demo.gallery.R;
 import com.tencent.shadow.demo.gallery.cases.entity.UseCase;
 import com.tencent.shadow.demo.gallery.util.ToastUtil;
+import com.tencent.shadow.demo.usecases.BaseAndroidTestActivity;
 
-public class TestStartServiceActivity extends BaseActivity {
+public class TestStartServiceActivity extends BaseAndroidTestActivity {
 
     public static class Case extends UseCase {
         @Override
@@ -37,18 +42,26 @@ public class TestStartServiceActivity extends BaseActivity {
 
     private TestService.MyLocalServiceBinder binder;
 
+    private TextView mTextView;
+
+    public final static String INTENT_ACTION = "com.tencent.shadow.test.service";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         serviceIntent = new Intent(this, TestService.class);
         setContentView(R.layout.layout_service);
+        mTextView = findViewById(R.id.tv_msg);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mBroadcastReceiver,new IntentFilter(INTENT_ACTION));
     }
 
     public void start(View view) {
+        mIdlingResource.setIdleState(false);
         startService(serviceIntent);
     }
 
     public void bind(View view) {
+        mIdlingResource.setIdleState(false);
         bindService(serviceIntent, serviceConnection, Service.BIND_AUTO_CREATE);
     }
 
@@ -65,11 +78,13 @@ public class TestStartServiceActivity extends BaseActivity {
     };
 
     public void stop(View view) {
-       stopService(serviceIntent);
+        mIdlingResource.setIdleState(false);
+        stopService(serviceIntent);
     }
 
     public void unbind(View view) {
-       unbindService(serviceConnection);
+        mIdlingResource.setIdleState(false);
+        unbindService(serviceConnection);
     }
 
     public void testBinder(View view) {
@@ -79,4 +94,19 @@ public class TestStartServiceActivity extends BaseActivity {
             binder.getMyLocalService().test();
         }
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver);
+    }
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String text = intent.getStringExtra("result");
+            mTextView.setText(text);
+            mIdlingResource.setIdleState(true);
+        }
+    };
 }
