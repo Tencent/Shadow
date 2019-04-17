d24913e20012904ae1f79435865e82deb75d6f4c
guok2046 <guok2046@gmail.com>
2019-04-17 21:08:01 +0800

    实现application的oncreate方法调用检测的单元测试

--- projects/demo/plugin-app/demo-main/src/main/java/com/tencent/shadow/demo/gallery/DemoApplication.java
+++ projects/demo/plugin-app/demo-main/src/main/java/com/tencent/shadow/demo/gallery/DemoApplication.java
@@ -6,10 +6,19 @@ import com.tencent.shadow.demo.gallery.cases.UseCaseManager;
 
 public class DemoApplication extends Application {
 
+    private static DemoApplication sInstence;
+
+    public boolean isOnCreate;
 
     @Override
     public void onCreate() {
+        sInstence = this;
+        isOnCreate = true;
         super.onCreate();
         UseCaseManager.initCase();
+    }
+
+    public static DemoApplication getInstance(){
+        return sInstence;
     }
 }
