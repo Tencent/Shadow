f81fcad32f4513ea29d16d8f6ce20e240a3007f0
shifujun <shifujun@foxmail.com>
2019-05-16 16:19:21 +0800

    重新组织test目录中工程目录结构。

--- projects/test/lib/constant/src/main/java/com/tencent/shadow/demo/testutil/Constant.java
+++ projects/test/lib/constant/src/main/java/com/tencent/shadow/demo/testutil/Constant.java
@@ -5,9 +5,9 @@ final public class Constant {
     public static final String KEY_ACTIVITY_CLASSNAME = "KEY_ACTIVITY_CLASSNAME";
     public static final String KEY_EXTRAS = "KEY_EXTRAS";
     public static final String KEY_PLUGIN_PART_KEY = "KEY_PLUGIN_PART_KEY";
-    public static final String PART_KEY_PLUGIN_MAIN_APP = "plugin-main-app";
-    public static final String PART_KEY_MULTIDEX_V1_0_2 = "plugin-multidex-v1_0_2";
-    public static final String PART_KEY_MULTIDEX_V2_0_1 = "plugin-multidex-v2_0_1";
+    public static final String PART_KEY_PLUGIN_MAIN_APP = "test-plugin-general-cases";
+    public static final String PART_KEY_MULTIDEX_V1_0_2 = "test-plugin-multidex-v1_0_2";
+    public static final String PART_KEY_MULTIDEX_V2_0_1 = "test-plugin-multidex-v2_0_1";
     public static final int FROM_ID_NOOP = 1000;
     public static final int FROM_ID_START_ACTIVITY = 1002;
 }
