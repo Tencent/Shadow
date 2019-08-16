4b187415126fb8defd1b04b5baa22db9704e6040
linxueyuanstdio <761516186@qq.com>
2019-08-16 19:17:42 +0800

    增加 Activity 的 onCreateOptionsMenu 测试用例

--- projects/sample/source/sample-plugin/sample-app-lib/src/main/java/com/tencent/shadow/sample/plugin/app/lib/usecases/activity/TestActivityOptionMenu.java
+++ projects/sample/source/sample-plugin/sample-app-lib/src/main/java/com/tencent/shadow/sample/plugin/app/lib/usecases/activity/TestActivityOptionMenu.java
@@ -19,58 +19,43 @@
 package com.tencent.shadow.sample.plugin.app.lib.usecases.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
+import android.view.Menu;
 
 import com.tencent.shadow.sample.plugin.app.lib.R;
 import com.tencent.shadow.sample.plugin.app.lib.gallery.cases.entity.UseCase;
-import com.tencent.shadow.sample.plugin.app.lib.gallery.util.ToastUtil;
 
-public class TestActivitySetTheme extends Activity {
+public class TestActivityOptionMenu extends Activity {
 
     public static class Case extends UseCase {
         @Override
         public String getName() {
-            return "Activity 主题测试";
+            return "Activity Menu测试";
         }
 
         @Override
         public String getSummary() {
-            return "测试Activity的 setTheme 方法";
+            return "测试Activity的 onCreateOptionsMenu";
         }
 
         @Override
         public Class getPageClass() {
-            return TestActivitySetTheme.class;
+            return TestActivityOptionMenu.class;
         }
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        int currentTheme = getIntent().getIntExtra("theme", 0);
-        currentTheme++;
-        setTheme(currentTheme % 2 == 0 ? R.style.TestPluginTheme : R.style.PluginAppThemeLight);
-        ToastUtil.showToast(TestActivitySetTheme.this, currentTheme % 2 == 0 ? "R.style.TestPluginTheme" : "R.style.PluginAppThemeLight");
-        //setTheme必须在super.onCreate之前调用才行
+        setTheme(R.style.PluginAppThemeLight);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activity_settheme);
-        final View btn = findViewById(R.id.button);
-        btn.setEnabled(true);
-        final int finalCurrentTheme = currentTheme;
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btn.setEnabled(false);
-
-                Intent intent = new Intent(TestActivitySetTheme.this, TestActivitySetTheme.class);
-                intent.putExtra("theme", finalCurrentTheme);
-                startActivity(intent);
-
-                btn.setEnabled(true);
-            }
-        });
+        setTitle("看右边的 menu ->");
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.case_test_activity_option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
