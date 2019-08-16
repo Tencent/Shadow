9a92187abac5236d7f58e0bf9ddf51d4ce82e228
linxueyuanstdio <761516186@qq.com>
2019-08-16 17:00:30 +0800

    Update TestActivitySetTheme.java

--- projects/sample/source/sample-plugin/sample-app-lib/src/main/java/com/tencent/shadow/sample/plugin/app/lib/usecases/activity/TestActivitySetTheme.java
+++ projects/sample/source/sample-plugin/sample-app-lib/src/main/java/com/tencent/shadow/sample/plugin/app/lib/usecases/activity/TestActivitySetTheme.java
@@ -19,6 +19,7 @@
 package com.tencent.shadow.sample.plugin.app.lib.usecases.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -50,18 +51,24 @@ public class TestActivitySetTheme extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        int currentTheme = getIntent().getIntExtra("theme", 0);
+        currentTheme++;
+        setTheme(currentTheme % 2 == 0 ? R.style.TestPluginTheme : R.style.PluginAppThemeLight);
+        ToastUtil.showToast(TestActivitySetTheme.this, currentTheme % 2 == 0 ? "R.style.TestPluginTheme" : "R.style.PluginAppThemeLight");
+        //setTheme必须在super.onCreate之前调用才行
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activity_settheme);
         final View btn = findViewById(R.id.button);
         btn.setEnabled(true);
+        final int finalCurrentTheme = currentTheme;
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btn.setEnabled(false);
 
-                currentTheme++;
-                setTheme(currentTheme % 2 == 0 ? R.style.TestPluginTheme : R.style.PluginAppThemeLight);
-                ToastUtil.showToast(TestActivitySetTheme.this, currentTheme % 2 == 0 ? "R.style.TestPluginTheme" : "R.style.PluginAppThemeLight");
+                Intent intent = new Intent(TestActivitySetTheme.this, TestActivitySetTheme.class);
+                intent.putExtra("theme", finalCurrentTheme);
+                startActivity(intent);
 
                 btn.setEnabled(true);
             }
