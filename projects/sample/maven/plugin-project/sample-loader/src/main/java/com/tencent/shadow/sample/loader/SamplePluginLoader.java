47e225982cbbaad5cf3c0c8d05d8e0452355e723
shifujun <shifujun@foxmail.com>
2019-10-30 16:11:19 +0800

    修复因sdk删除Report接口导致的编译错误。

--- projects/sample/maven/plugin-project/sample-loader/src/main/java/com/tencent/shadow/sample/loader/SamplePluginLoader.java
+++ projects/sample/maven/plugin-project/sample-loader/src/main/java/com/tencent/shadow/sample/loader/SamplePluginLoader.java
@@ -2,7 +2,6 @@ package com.tencent.shadow.sample.loader;
 
 import android.content.Context;
 
-import com.tencent.shadow.core.loader.Reporter;
 import com.tencent.shadow.core.loader.ShadowPluginLoader;
 import com.tencent.shadow.core.loader.managers.ComponentManager;
 import com.tencent.shadow.sample.loader.SampleComponentManager;
@@ -27,23 +26,4 @@ public class SamplePluginLoader extends ShadowPluginLoader {
         return componentManager;
     }
 
-    @Override
-    public Reporter getMExceptionReporter() {
-        return new Reporter() {
-            @Override
-            public void reportException(Exception exception) {
-                android.util.Log.e(TAG, "reportException", exception);
-            }
-
-            @Override
-            public void log(String msg) {
-                android.util.Log.i(TAG, msg);
-            }
-        };
-    }
-
-    @Override
-    public String getMAbi() {
-        return "";
-    }
 }
