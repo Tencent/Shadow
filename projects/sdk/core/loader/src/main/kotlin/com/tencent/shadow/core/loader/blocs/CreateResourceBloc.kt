d984d3aa2f0cee12806c9a48279dde38802f0da4
shifujun <shifujun@foxmail.com>
2019-11-19 21:51:35 +0800

    fix(core.loader): 修复WebView中使用select标签会Crash的问题

--- projects/sdk/core/loader/src/main/kotlin/com/tencent/shadow/core/loader/blocs/CreateResourceBloc.kt
+++ projects/sdk/core/loader/src/main/kotlin/com/tencent/shadow/core/loader/blocs/CreateResourceBloc.kt
@@ -22,12 +22,25 @@ import android.content.Context
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.content.res.Resources
+import android.os.Handler
+import android.os.Looper
+import android.webkit.WebView
+import java.util.concurrent.CountDownLatch
 
 object CreateResourceBloc {
     fun create(packageArchiveInfo: PackageInfo, archiveFilePath: String, hostAppContext: Context): Resources {
+        //先用宿主context初始化一个WebView，以便WebView的逻辑去修改sharedLibraryFiles，将webview.apk添加进去
+        val latch = CountDownLatch(1)
+        Handler(Looper.getMainLooper()).post {
+            WebView(hostAppContext)
+            latch.countDown()
+        }
+        latch.await()
+
         val packageManager = hostAppContext.packageManager
         packageArchiveInfo.applicationInfo.publicSourceDir = archiveFilePath
         packageArchiveInfo.applicationInfo.sourceDir = archiveFilePath
+        packageArchiveInfo.applicationInfo.sharedLibraryFiles = hostAppContext.applicationInfo.sharedLibraryFiles
         try {
             return packageManager.getResourcesForApplication(packageArchiveInfo.applicationInfo)
         } catch (e: PackageManager.NameNotFoundException) {
