a17bab9ada37007266de655169dcda7d0de2e433
shifujun <shifujun@foxmail.com>
2019-12-27 16:09:09 +0800

    fix(activity-container): 添加API21引入的类型的假定义
    通过https://developer.android.com/sdk/api_diff/21/changes找出新增的接口涉及的新类型
    
    #230

---
+++ projects/sdk/core/activity-container/src/main/java/android/app/ActivityManager.java
@@ -0,0 +1,7 @@
+package android.app;
+
+public interface ActivityManager {
+    //避免API 21以下系统找不到这个类
+    interface TaskDescription {
+    }
+}
---
+++ projects/sdk/core/activity-container/src/main/java/android/app/SharedElementCallback.java
@@ -0,0 +1,5 @@
+package android.app;
+
+//避免API 21以下系统找不到这个类
+public interface SharedElementCallback {
+}
