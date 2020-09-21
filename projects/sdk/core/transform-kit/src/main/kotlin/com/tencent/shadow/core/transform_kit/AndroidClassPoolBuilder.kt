e160ba786b218010d566d5a2fb1390ddc03a0e42
shifujun <shifujun@foxmail.com>
2020-09-21 18:14:35 +0800

    fix(transform): rebuildStackMap时自动创建找不到的类型
    CodeConverter在重写Method实现时需要rebuildStackMap。在这个过程中，只有涉及泛型具有多个下界时，
    Javassist试图帮我们找出多个下界的共同父类型作为新的下界。这时才会需要加载CtClass。
    所以，并不是所有找不到的类型都会引起rebuildStackMap失败。
    
    由于我们的逻辑不会涉及修改找不到的类型的行为、甚至修改它们的父类型，
    所以在这种场景下直接将找不到的类型构造出来，即假设它们的父类是Object类型。
    
    fix #411

--- projects/sdk/core/transform-kit/src/main/kotlin/com/tencent/shadow/core/transform_kit/AndroidClassPoolBuilder.kt
+++ projects/sdk/core/transform-kit/src/main/kotlin/com/tencent/shadow/core/transform_kit/AndroidClassPoolBuilder.kt
@@ -34,7 +34,7 @@ class AndroidClassPoolBuilder(
     override fun build(): ClassPool {
         //这里使用useDefaultPath:false是因为这里取到的contextClassLoader不包含classpath指定进来的runtime
         //所以在外部先获取一个包含了runtime的contextClassLoader传进来
-        val classPool = ClassPool(false)
+        val classPool = AutoMakeMissingClassPool(false)
 
         classPool.appendClassPath(LoaderClassPath(contextClassLoader))
         if (logger.isInfoEnabled && contextClassLoader is VisitableURLClassLoader) {
---
+++ projects/sdk/core/transform-kit/src/main/kotlin/com/tencent/shadow/core/transform_kit/AutoMakeMissingClassPool.kt
@@ -0,0 +1,28 @@
+package com.tencent.shadow.core.transform_kit
+
+import javassist.ClassPool
+import javassist.CtClass
+
+class AutoMakeMissingClassPool(useDefaultPath: Boolean) : ClassPool(useDefaultPath) {
+
+    override fun get0(classname: String?, useCache: Boolean): CtClass? {
+        var get0 = super.get0(classname, useCache)
+
+        // 来自javassist.bytecode.stackmap.TypeData.TypeVar.fixTypes2的调用时，
+        // 如果类不存在，就构造一个。
+        // fixTypes2是重建StackMap的步骤，参考：https://stackoverflow.com/a/37310409/11616914
+        // 我们的Transform不会去修改找不到的类型相关的代码，
+        // 而fixTypes2处理的逻辑是在确定泛型的下界，
+        // 由于我们没改任何跟找不到类型相关的逻辑，所以未知类型的父类重定义为Object，应该没有危险。
+        //
+        // 这里必须判断是来自的fixTypes2的调用，而不是所有调用都构造类型，是因为存在像
+        // javassist.compiler.MemberResolver.lookupClass0
+        // 依赖NotFoundException的逻辑存在。
+        if (get0 == null && Throwable().stackTrace[2].methodName == "fixTypes2") {
+            get0 = makeClass(classname)
+            if (useCache) cacheCtClass(get0.getName(), get0, false)
+        }
+
+        return get0
+    }
+}
