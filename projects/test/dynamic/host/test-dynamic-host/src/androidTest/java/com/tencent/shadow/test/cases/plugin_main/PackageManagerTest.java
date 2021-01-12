f7b8dfac1da2e0635c0b19c3354a9b20ed987c3e
shifujun <shifujun@foxmail.com>
2021-01-12 18:39:21 +0800

    test: 重构TestPackageManagerActivity为自动化测试
    general-cases-app中新增以正常App方式执行测试代码，方便获取没有Shadow干预时测试用例的执行结果。
    
    #464

---
+++ projects/test/dynamic/host/test-dynamic-host/src/androidTest/java/com/tencent/shadow/test/cases/plugin_main/PackageManagerTest.java
@@ -0,0 +1,39 @@
+package com.tencent.shadow.test.cases.plugin_main;
+
+import android.content.Intent;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import com.tencent.shadow.test.lib.test_manager.TestManager;
+
+import org.junit.Test;
+
+public class PackageManagerTest extends PluginMainAppTest {
+
+    @Override
+    protected Intent getLaunchIntent() {
+        Intent pluginIntent = new Intent();
+        String packageName = ApplicationProvider.getApplicationContext().getPackageName();
+        pluginIntent.setClassName(
+                packageName,
+                "com.tencent.shadow.test.plugin.general_cases.lib.usecases.packagemanager.TestPackageManagerActivity"
+        );
+        return pluginIntent;
+    }
+
+    @Test
+    public void testGetApplicationInfoClassName() {
+        matchTextWithViewTag("getApplicationInfo/className", "com.tencent.shadow.test.plugin.general_cases.lib.gallery.TestApplication");
+    }
+
+    @Test
+    public void testGetApplicationInfoNativeLibraryDir() {
+        matchSubstringWithViewTag("getApplicationInfo/nativeLibraryDir", TestManager.uuid + "_lib");
+    }
+
+    @Test
+    public void testGetApplicationInfoMetaData() {
+        matchTextWithViewTag("getApplicationInfo/metaData", "test_value");
+    }
+
+}
