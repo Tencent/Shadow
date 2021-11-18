4f3a95374bbffb9c6a245e606c7615cce0e93224
shifujun <shifujun@foxmail.com>
2021-11-18 16:59:04 +0800

    chore: 删除sunflower sample

--- projects/sample/sunflower/plugin-project/sample-runtime/src/main/java/com/tencent/shadow/sample/runtime/PluginDefaultProxyActivity.java
+++
@@ -1,7 +0,0 @@
-package com.tencent.shadow.sample.runtime;
-
-
-import com.tencent.shadow.core.runtime.container.PluginContainerActivity;
-
-public class PluginDefaultProxyActivity extends PluginContainerActivity {
-}
