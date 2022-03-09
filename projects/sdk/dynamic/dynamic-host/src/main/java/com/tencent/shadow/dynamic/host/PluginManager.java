ee1d92fea2af3f2571a4285d3ff7be2d39fb562e
shifujun <shifujun@foxmail.com>
2022-03-09 14:40:33 +0800

    chore(dynamic.dynamic-host): fix typo
    fromId

--- projects/sdk/dynamic/dynamic-host/src/main/java/com/tencent/shadow/dynamic/host/PluginManager.java
+++ projects/sdk/dynamic/dynamic-host/src/main/java/com/tencent/shadow/dynamic/host/PluginManager.java
@@ -30,9 +30,9 @@ public interface PluginManager {
 
     /**
      * @param context  context
-     * @param formId   标识本次请求的来源位置，用于区分入口
+     * @param fromId   标识本次请求的来源位置，用于区分入口
      * @param bundle   参数列表
      * @param callback 用于从PluginManager实现中返回View
      */
-    void enter(Context context, long formId, Bundle bundle, EnterCallback callback);
+    void enter(Context context, long fromId, Bundle bundle, EnterCallback callback);
 }
