df46691f6531a5123e79d116722f3c4c394b5533
shifujun <shifujun@foxmail.com>
2022-03-14 13:10:03 +0800

    fix(core.runtime): BroadcastReceiver与BroadcastReceiverWapper映射Map采用弱引用实现
    通过分析代理对象的使用场景可以确定这个映射Map可以由弱引用实现，
    从而避免对原有BroadcastReceiver的GC时机造成影响。
    
    该为弱引用实现后，可以省去对map的remove操作，修复此前unregisterReceiver时忘记remove造成的泄漏。

--- projects/sdk/core/runtime/src/main/java/com/tencent/shadow/core/runtime/BroadcastReceiverWrapper.java
+++ projects/sdk/core/runtime/src/main/java/com/tencent/shadow/core/runtime/BroadcastReceiverWrapper.java
@@ -4,13 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class BroadcastReceiverWapper extends BroadcastReceiver {
+public class BroadcastReceiverWrapper extends BroadcastReceiver {
 
     final private BroadcastReceiver mRealBroadcastReceiver;
 
     final private ShadowContext mShadowContext;
 
-    public BroadcastReceiverWapper(BroadcastReceiver broadcastReceiver, ShadowContext shadowContext) {
+    public BroadcastReceiverWrapper(BroadcastReceiver broadcastReceiver, ShadowContext shadowContext) {
         mRealBroadcastReceiver = broadcastReceiver;
         mShadowContext = shadowContext;
     }
