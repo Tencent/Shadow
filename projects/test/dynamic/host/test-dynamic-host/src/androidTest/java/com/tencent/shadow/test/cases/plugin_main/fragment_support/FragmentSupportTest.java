797177c936524bff521bd089a9e6c3acf309956a
shifujun <shifujun@foxmail.com>
2022-05-06 15:35:10 +0800

    test: 全量自动化测试时忽略Suite测试
    避免它们引用的测试被重复测试两次。

--- projects/test/dynamic/host/test-dynamic-host/src/androidTest/java/com/tencent/shadow/test/cases/plugin_main/fragment_support/FragmentSupportTest.java
+++ projects/test/dynamic/host/test-dynamic-host/src/androidTest/java/com/tencent/shadow/test/cases/plugin_main/fragment_support/FragmentSupportTest.java
@@ -1,9 +1,11 @@
 package com.tencent.shadow.test.cases.plugin_main.fragment_support;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
+@Ignore("避免自动化全量测试时重复这些测试")
 @Suite.SuiteClasses({
         ProgrammaticallyAddNormalFragmentTest.class,
         ProgrammaticallyAddSubFragmentTest.class,
