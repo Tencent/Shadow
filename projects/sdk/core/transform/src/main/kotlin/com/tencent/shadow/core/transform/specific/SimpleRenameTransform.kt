8971967bcb6b5718d3a150f42d6a4a9f30814a59
shifujun <shifujun@foxmail.com>
2022-05-06 17:34:55 +0800

    perf(core.transform): SimpleRenameTransform避免allInputClass作为filter
    排除那些根本和当前Transform涉及的类没关系的类。

--- projects/sdk/core/transform/src/main/kotlin/com/tencent/shadow/core/transform/specific/SimpleRenameTransform.kt
+++ projects/sdk/core/transform/src/main/kotlin/com/tencent/shadow/core/transform/specific/SimpleRenameTransform.kt
@@ -26,7 +26,8 @@ import javassist.CtClass
 open class SimpleRenameTransform(private val fromToMap: Map<String, String>) : SpecificTransform() {
     final override fun setup(allInputClass: Set<CtClass>) {
         newStep(object : TransformStep {
-            override fun filter(allInputClass: Set<CtClass>) = allInputClass
+            override fun filter(allInputClass: Set<CtClass>) =
+                filterRefClasses(allInputClass, fromToMap.keys.toList())
 
             override fun transform(ctClass: CtClass) {
                 fromToMap.forEach {
