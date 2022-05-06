dcbd6c542861f734065e0021eec0fe0ffa355bf3
shifujun <shifujun@foxmail.com>
2022-05-06 15:35:10 +0800

    test: 全量自动化测试时忽略Suite测试
    避免它们引用的测试被重复测试两次。

--- projects/test/dynamic/host/test-dynamic-host/src/androidTest/java/com/tencent/shadow/test/cases/plugin_main/application_info/ApplicationInfoTest.java
+++ projects/test/dynamic/host/test-dynamic-host/src/androidTest/java/com/tencent/shadow/test/cases/plugin_main/application_info/ApplicationInfoTest.java
@@ -1,9 +1,11 @@
 package com.tencent.shadow.test.cases.plugin_main.application_info;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
+@Ignore("避免自动化全量测试时重复这些测试")
 @Suite.SuiteClasses({ContextGetApplicationInfoTest.class,
         PackageManagerGetSelfApplicationInfoTest.class,
         PackageManagerGetOtherInstalledApplicationInfoTest.class})
