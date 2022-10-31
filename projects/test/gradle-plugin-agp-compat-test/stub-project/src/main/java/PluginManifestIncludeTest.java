fb93248e55e8201b021eb0c7900bbd51c578afa2
shifujun <shifujun@foxmail.com>
2022-10-31 18:03:06 +0800

    fix(core.gradle-plugin): 改正添加PluginManifest.java到源码路径的实现
    改正前只有在AGP低至3.2.0或高至7.2.2才会出现问题。
    
    改正方法是基于调试得出，未能查到两种方法区别的理论依据。由于有自动化测试验证，故放心修改。
    
    fix #1074
    fix #1047

---
+++ projects/test/gradle-plugin-agp-compat-test/stub-project/src/main/java/PluginManifestIncludeTest.java
@@ -0,0 +1,7 @@
+/**
+ * 引用一下PluginManifest类型，测试自动生成的
+ * PluginManifest.java 是否被添加到编译源码路径中了
+ */
+public final class PluginManifestIncludeTest {
+    public static final Class DEBUG = com.tencent.shadow.core.manifest_parser.PluginManifest.class;
+}
